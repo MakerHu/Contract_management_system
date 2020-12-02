000a917346e01401fba2b9640cdc8ff9c98d11ab
BJTU18301126 <1003964217@qq.com>
2020-12-02 13:07:54 +0800

    胡江浩 设计了主界面侧边栏和顶部

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+function list() {
+    var body = document.getElementsByTagName("body")[0]
+    var aside = document.getElementsByTagName("aside")[0]
+    if (aside.clientWidth <= 40) {
+        body.className = ""
+    } else if (aside.clientWidth > 40) {
+        console.log("side")
+        body.className = "minibody"
+    }
+}
