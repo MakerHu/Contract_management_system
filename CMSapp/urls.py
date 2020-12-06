93a8ef04d86df824f58d09634cff3d1f6ecd9260
deng-shuo <ddll841022@qq.com>
2020-12-06 15:53:53 +0800

    邓硕实现了合同分配部分功能2

--- CMSapp/urls.py
+++ CMSapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from CMSapp import views
 from CMSapp import contract_management
+from CMSapp import data_management
 
 urlpatterns = [
     # 主页
@@ -32,4 +33,5 @@ urlpatterns = [
     path('ajax_login/', views.ajax_login),
     path('ajax_register/', views.ajax_register),
     path('ajax_confirm_username/', views.ajax_confirm_username),
+    path('ajax_distribution/', data_management.test),
 ]
