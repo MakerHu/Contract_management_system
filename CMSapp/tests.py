8938067bcac012319d81da1d2b045a074b55b9cc
xzc0605 <1062469890@qq.com>
2020-12-06 15:07:18 +0800

    徐紫程添加会签、已定稿、流程查询部分

--- CMSapp/tests.py
+++ CMSapp/tests.py
@@ -18,9 +18,11 @@ from CMSapp import models
 # models.user.objects.all().delete()
 # print(models.user.objects.all()[0].username)
 print(models.user.objects.all())
-models.role.objects.create(rolename='newuser', description='新用户').save()
+models.customer.objects.get_or_create(cusid=1, cusname='客户1', address='福建', tel='13156')
+cusEntity = models.customer.objects.filter(cusid=1)[0]
+userEntity = models.user.objects.filter(username='xzcnb')[0]
+models.contract.objects.get_or_create(conname='合同1',username=userEntity, cusid=cusEntity,begintime='2020-05-03',endtime='2020-10-12')
+contractEntity = models.contract.objects.filter(conid=1)[0]
+models.contract_state.objects.get_or_create(type=1,conid=contractEntity)
 
-conidentity = models.contract.objects.get(conid=1)
-models.contract_state.objects.create(type=2,conid=conidentity).save()
-models.customer.objects.create(cusname='客户1', address='福建',tel='13156').save()
-models.contract.objects.create(conname='合同1',cusid=2,begintime='2020-05-03',endtime='2020-10-12')
+
